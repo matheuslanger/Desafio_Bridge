d0ad73aca32d372af22916904bab580b75fe0189
Matheus Langer <matheus.araujo.langer@gmail.com>
2022-08-09 12:14:45 -0300

    Adiciona tela inicial com dados mockados

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import App from './components/App';
 
 ReactDOM.render(
-    <h1 >
-        Bridge Challenge
-    </h1>,
+    <App />,
     document.getElementById('root')
-)
+);
