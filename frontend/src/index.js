d547906d505d8073d6e64a71cc4533517d0a41e1
Matheus Langer <matheus.araujo.langer@gmail.com>
2022-08-09 10:18:04 -0300

    Adiciona base React

---
+++ frontend/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+    <h1 >
+        Bridge Challenge
+    </h1>,
+    document.getElementById('root')
+)
