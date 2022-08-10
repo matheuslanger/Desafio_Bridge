631a891ae918e66190491bf298de3646b032ca5f
Matheus Langer <matheus.araujo.langer@gmail.com>
2022-08-09 22:46:42 -0300

    Permite requisições de todas as origens

--- src/main/java/com/bridge/desafio_bridge/num/AlgorithmController.java
+++ src/main/java/com/bridge/desafio_bridge/num/AlgorithmController.java
@@ -3,6 +3,7 @@ package com.bridge.desafio_bridge.num;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(path = "api/v1/numbers")
 public class AlgorithmController
